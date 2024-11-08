8f0d8bd52eac56a4d885fae11a9c3b8520531f1a
Priyam22r <ruchi.gupta48@gmail.com>
2024-11-08 10:45:46 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    const popupMenu = document.getElementById("popupMenu");
+    popupMenu.classList.toggle("active");
+}           
+
+const testimonials = document.querySelectorAll('.testimonial');
+let currentTestimonial = 0;
+
+function showNextTestimonial() {
+    testimonials[currentTestimonial].classList.remove('active');
+    currentTestimonial = (currentTestimonial + 1) % testimonials.length;
+    testimonials[currentTestimonial].classList.add('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    testimonials[0].classList.add('active');
+    setInterval(showNextTestimonial, 10000); // Change testimonial every 10 seconds
+});
